c9f92919301c8512e5a8b83761bdfce7ed7d9d1c
Erick Fuentes <fuentes.erick@gmail.com>
2016-01-19 23:52:05 -0500

    Initial Commit

---
+++ drivers/NeoPixelDriver_v1_0/src/NeoPixelDriver.h
@@ -0,0 +1,83 @@
+
+#ifndef NEOPIXELDRIVER_H
+#define NEOPIXELDRIVER_H
+
+
+/****************** Include Files ********************/
+#include "xil_types.h"
+#include "xstatus.h"
+
+#define NEOPIXELDRIVER_S00_AXI_SLV_REG0_OFFSET 0
+#define NEOPIXELDRIVER_S00_AXI_SLV_REG1_OFFSET 4
+#define NEOPIXELDRIVER_S00_AXI_SLV_REG2_OFFSET 8
+#define NEOPIXELDRIVER_S00_AXI_SLV_REG3_OFFSET 12
+#define NEOPIXELDRIVER_S00_AXI_SLV_REG4_OFFSET 16
+#define NEOPIXELDRIVER_S00_AXI_SLV_REG5_OFFSET 20
+#define NEOPIXELDRIVER_S00_AXI_SLV_REG6_OFFSET 24
+#define NEOPIXELDRIVER_S00_AXI_SLV_REG7_OFFSET 28
+
+
+/**************************** Type Definitions *****************************/
+/**
+ *
+ * Write a value to a NEOPIXELDRIVER register. A 32 bit write is performed.
+ * If the component is implemented in a smaller width, only the least
+ * significant data is written.
+ *
+ * @param   BaseAddress is the base address of the NEOPIXELDRIVERdevice.
+ * @param   RegOffset is the register offset from the base to write to.
+ * @param   Data is the data written to the register.
+ *
+ * @return  None.
+ *
+ * @note
+ * C-style signature:
+ * 	void NEOPIXELDRIVER_mWriteReg(u32 BaseAddress, unsigned RegOffset, u32 Data)
+ *
+ */
+#define NEOPIXELDRIVER_mWriteReg(BaseAddress, RegOffset, Data) \
+  	Xil_Out32((BaseAddress) + (RegOffset), (u32)(Data))
+
+/**
+ *
+ * Read a value from a NEOPIXELDRIVER register. A 32 bit read is performed.
+ * If the component is implemented in a smaller width, only the least
+ * significant data is read from the register. The most significant data
+ * will be read as 0.
+ *
+ * @param   BaseAddress is the base address of the NEOPIXELDRIVER device.
+ * @param   RegOffset is the register offset from the base to write to.
+ *
+ * @return  Data is the data from the register.
+ *
+ * @note
+ * C-style signature:
+ * 	u32 NEOPIXELDRIVER_mReadReg(u32 BaseAddress, unsigned RegOffset)
+ *
+ */
+#define NEOPIXELDRIVER_mReadReg(BaseAddress, RegOffset) \
+    Xil_In32((BaseAddress) + (RegOffset))
+
+/************************** Function Prototypes ****************************/
+/**
+ *
+ * Run a self-test on the driver/device. Note this may be a destructive test if
+ * resets of the device are performed.
+ *
+ * If the hardware system is not built correctly, this function may never
+ * return to the caller.
+ *
+ * @param   baseaddr_p is the base address of the NEOPIXELDRIVER instance to be worked on.
+ *
+ * @return
+ *
+ *    - XST_SUCCESS   if all self-test code passed
+ *    - XST_FAILURE   if any self-test code failed
+ *
+ * @note    Caching must be turned off for this function to work.
+ * @note    Self test may fail if data memory and device are not on the same bus.
+ *
+ */
+XStatus NEOPIXELDRIVER_Reg_SelfTest(void * baseaddr_p);
+
+#endif // NEOPIXELDRIVER_H
---
+++ hdl/NeoPixelDriver.vhd
@@ -0,0 +1,84 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+use IEEE.MATH_REAL.ALL;
+
+entity NeoPixelDriver is
+    Generic (
+        clk_rate : integer := 50000000);
+    Port ( clk : in STD_LOGIC;
+           en : in STD_LOGIC;
+           dataIn : in STD_LOGIC_VECTOR(23 downto 0);
+           dataOut : out STD_LOGIC:= '0';
+           isEmpty: out STD_LOGIC := '1';
+           busy: out STD_LOGIC := '0');
+end NeoPixelDriver;
+
+architecture Behavioral of NeoPixelDriver is
+-- State definitions
+    constant state_idle     : unsigned(1 downto 0) := "00";
+    constant state_transmit : unsigned(1 downto 0) := "01";
+    constant state_finish   : unsigned(1 downto 0) := "10";
+    
+-- other constants
+    constant bitTime : integer := integer(floor(real(clk_rate) / 800000.0));
+    constant zeroTime : integer := integer(floor(real(clk_rate) / 800000.0 * 0.3));
+    constant oneTime : integer := integer(floor(real(clk_rate) / 800000.0 * 0.6));
+    constant resetTime : integer := integer(floor(real(clk_rate) / 20000.0));
+      
+    signal state : unsigned (1 downto 0) := state_idle;
+    signal dataCopy : STD_LOGIC_VECTOR (23 downto 0) := (others => '0');
+    signal bitCounter : integer := 0;
+    signal bitTimeCounter : integer := 0;
+begin
+    fsm_proc: process(clk)
+    begin
+        if rising_edge(clk) then
+            case state is
+                when state_idle =>
+                    if en = '1' then
+                        dataCopy <= dataIn;
+                        isEmpty <= '0';
+                        bitCounter <= 0;
+                        bitTimeCounter <= 0;
+                        state <= state_transmit;
+                        dataOut <= '1';
+                        busy <= '1';
+                    end if;
+                when state_transmit =>
+                    bitTimeCounter <= bitTimeCounter + 1;
+                                        
+                    if bitTimeCounter = bitTime then
+                        bitTimeCounter <= 0;
+                        bitCounter <= bitCounter + 1;
+                        dataOut <= '1';
+                        isEmpty <= '1';
+                        -- Weird scheduling issue where bit counter
+                        -- does not get updated until the next iteration
+                        if bitCounter = 23 and en = '1' then
+                            state <= state_idle;
+                        elsif bitCounter = 23 then
+                            dataOut <= '0';
+                            state <= state_finish;
+                        end if;
+                    end if;
+                    
+                    if (dataCopy(23 - bitCounter) = '0' and bitTimeCounter = zeroTime) or
+                        (dataCopy(23 - bitCounter) = '1' and bitTimeCounter = oneTime) then
+                        dataOut <= '0';    
+                    end if; 
+
+                when state_finish =>
+                    bitTimeCounter <= bitTimeCounter + 1;
+                    if (bitTimeCounter = resetTime) then
+                        state <= state_idle;
+                        busy <= '0';
+                    end if;
+
+                when others =>
+            end case;
+        end if;
+    end process;
+
+end Behavioral;
